3fc7e71afafbffcf8100a6e9b8535ad7fe3d01b0
dududdu123131313 <2672106922@qq.com>
2024-12-12 14:06:25 +0800

    12.12

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': { // 使用路径前缀作为代理匹配规则，比如前端请求路径是 /api/doctors/department 这样的就会被匹配
-        target: 'http://10.3.112.10:8088',
+        target: 'http://113.44.151.153:8088',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '' 
